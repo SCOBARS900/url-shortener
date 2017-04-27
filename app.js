fa61f865a08e3c3cc739be4ba1831dbfa7ccb761
Julio Escobar <julioescobar900@gmail.com>
2017-04-27 09:47:31 -0300

    Add modules

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var useragent = require('express-useragent');
+
 
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(useragent.express());
+
 
 
 app.set('port', (process.env.PORT || 3000));
