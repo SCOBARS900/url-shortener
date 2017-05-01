34899795f34df8f46204df0bda79341cc1d4dfd5
Julio Escobar <julioescobar900@gmail.com>
2017-05-01 10:59:58 -0300

    add new modules and set server on port 3000

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
-
-
+var mongoose = require('mongoose');
 var app = express();
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -14,6 +13,12 @@ app.use(cors());
 app.set('port', (process.env.PORT || 3000));
 
 
+app.get('/new/:urlToShorten(*)', (req, res)=> {
+     
+  var urlToShorten = req.params.urlToShorten;
+  console.log(urlToShorten);
+
+});
 
 
 
