291130477820a750beead3f571e9c3d6d0d1f521
Julio Escobar <julioescobar900@gmail.com>
2017-05-02 11:30:05 -0300

    deploying mlab to project

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 if(process.env.MONGODB_URI) {
- mongoose.connect(process.env.MONGODB_URI);
+ mongoose.connect(process.env.MONGOLAB_URI);
 
 }else {
  var db = 'mongodb://localhost:27017/shortUrls';
